838afef4cd4dfb6e16d72361a3f65ec4c3e7807c
Aizhamal07 <m.aijamal07@icloud.com>
2020-04-24 13:32:05 -0500

    PositiveAsiyaGivesPositiveEnergy.java

---
+++ src/tests/PositiveAsiyaGivesPositiveEnergy.java
@@ -0,0 +1,9 @@
+package tests;
+
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+
+public class PositiveAsiyaGivesPositiveEnergy{
+    public static void main(String[] args) {
+        System.out.println("She is so cool");
+    }
+}
