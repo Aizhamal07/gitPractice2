0ec801ca0fb4f45bf28df77ddb70d60d220aaa71
Aizhamal07 <64108407+Aizhamal07@users.noreply.github.com>
2020-04-24 15:29:43 -0500

    Update Aylin.java

--- src/day01/Aylin.java
+++ src/day01/Aylin.java
@@ -3,5 +3,7 @@ package day01;
 public class Aylin {
     public static void main(String[] args) {
         System.out.println("Aylin, I love you my sunshine!");
+        //This line was added from github
+        
     }
 }
