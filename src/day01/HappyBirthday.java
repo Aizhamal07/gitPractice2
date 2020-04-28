5dd379ead047414e973a8b8c6b70b1fe884313e3
Aizhamal07 <64108407+Aizhamal07@users.noreply.github.com>
2020-04-28 14:50:40 -0500

    Update HappyBirthday.java
    I want to add description here

--- src/day01/HappyBirthday.java
+++ src/day01/HappyBirthday.java
@@ -3,5 +3,8 @@ package day01;
 public class HappyBirthday {
     public static void main(String[] args) {
         System.out.println("Happy Birthday to me!");
+        
+        //Now I am just checking if my changes gonna appear in IntelliJ
+        //I am learning GIT
     }
 }
